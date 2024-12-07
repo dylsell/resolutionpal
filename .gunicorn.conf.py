5b4e1f41fcc0abafea60e2747e642ef3bf4090ef
Dylan Sellberg <dylansellberg@gmail.com>
2024-12-07 15:21:52 -0500

    Add explicit Gunicorn configuration and cleanup

---
+++ .gunicorn.conf.py
@@ -0,0 +1,26 @@
+# Gunicorn configuration file
+import multiprocessing
+import os
+
+# Server socket
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 120
+keepalive = 2
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'debug'
+
+# Process naming
+proc_name = 'resolutionpal'
+
+# SSL
+keyfile = None
+certfile = None 
